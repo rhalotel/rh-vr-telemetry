0a5be8e25706aa357d21fbda1e66a4be256ae722
Radoslav Jizdny <rhalotel@rhalotel.com>
2019-10-26 16:47:36 +0200

    struktura pre konkretne vizualizacie .../apps/{viz-name}/rel_model.js

---
+++ src/js/apps/truck/rel_model.js
@@ -0,0 +1,15 @@
+/*
+TotalFuel* 0.001 L
+fuelLevel1/ 2.5 %
+Torque-125.0 %
+EngineHours*0.05 s
+EngineSpeed * 0.125 rpm 
+ enginePercentLoad*1 %
+VehicleHDistance * 5.0 / 1000 km
+tco1.VehicleSpeed / 256 km/h
+EngineCoolantTmp  - 40.0 C
+AmbientAirTemp* 0.03125 - 273.0 C
+breakPedalPosition  * 0.4 %
+combVeight 
+
+*/ 
