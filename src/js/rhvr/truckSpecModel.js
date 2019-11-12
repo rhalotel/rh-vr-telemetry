453a909bb0485dbe3cba508046b294c2bbe06c01
marcoBS335 <56341836+marcoBS335@users.noreply.github.com>
2019-11-12 14:58:59 +0100

    specModel - JSON, treba aj mixer(this,self)

--- src/js/rhvr/truckSpecModel.js
+++ src/js/rhvr/truckSpecModel.js
@@ -2,12 +2,13 @@ let specModel = {
     VehicleSpeed: function(d) 
     {
         speedWheel = 0; //pre speedWheel = 1, maju kolesa ~42RPM
+        speedFromData = d.tco1.VehicleSpeed / 256; //km/h
         // 1RPS je (2*pi*r)
         r = 0.538
         sw2rpm = 42
         kmph2mps = 1/3.6
         rps2mps = (2*pi*r); //= 3.38035369526m;
-        curr_mps = d*kmph2mps
+        curr_mps = speedFromData*kmph2mps
 
         currRPS = (1/rps2mps)*curr_mps			//vypocita momentalne RPS z rychlosti v mps a konstanty
         currRPM = currRPS*60					//	vypocita momentalne RPM
