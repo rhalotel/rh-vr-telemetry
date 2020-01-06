0fe37d6a5cc038da7422c7f4740359ee5e37cb8c
b-filip <56341886+b-filip@users.noreply.github.com>
2020-01-06 17:14:42 +0100

    Update houseSpecModel.js

--- examples/vis/energyHouse/houseSpecModel.js
+++ examples/vis/energyHouse/houseSpecModel.js
@@ -2,37 +2,37 @@ var house1 = {
     isSet : false,
     update: function(json,visItem) 
     {
-        {/* *START* Sun position based on time */
-            var sunObject = visItem.get3DObjectByName(["Sun"]);
-            sunLocationName = ["SunAction"];
-            sunLocationAnim = visItem.getAnimationByName(sunLocationName);
-            // average sunrise time for november in london: 7:18
-            var sunrise = "7:18".split(/[.:]/);
-            var hours = parseInt(sunrise[0], 10);
-            var minutes = sunrise[1] ? parseInt(sunrise[1], 10) : 0;
-            sunrise = hours + minutes / 60;
+        // {/* *START* Sun position based on time */
+        //     var sunObject = visItem.get3DObjectByName(["Sun"]);
+        //     sunLocationName = ["SunAction"];
+        //     sunLocationAnim = visItem.getAnimationByName(sunLocationName);
+        //     // average sunrise time for november in london: 7:18
+        //     var sunrise = "7:18".split(/[.:]/);
+        //     var hours = parseInt(sunrise[0], 10);
+        //     var minutes = sunrise[1] ? parseInt(sunrise[1], 10) : 0;
+        //     sunrise = hours + minutes / 60;
             
-            // average sunset time for november in london: 16:13
-            var sunset = "16:13".split(/[.:]/);
-            hours = parseInt(sunset[0], 10);
-            minutes = sunset[1] ? parseInt(sunset[1], 10) : 0;
-            sunset = hours + minutes / 60;
+        //     // average sunset time for november in london: 16:13
+        //     var sunset = "16:13".split(/[.:]/);
+        //     hours = parseInt(sunset[0], 10);
+        //     minutes = sunset[1] ? parseInt(sunset[1], 10) : 0;
+        //     sunset = hours + minutes / 60;
 
-            var currDate = new Date();
-            var currTime = (currDate.getHours() + ":" + currDate.getMinutes()).split(/[.:]/);
-            hours = parseInt(currTime[0], 10);
-            minutes = currTime[1] ? parseInt(currTime[1], 10) : 0;
-            currTime = hours + minutes / 60;
-            if (currTime>=sunrise && currTime <=sunset) {
-            	if (!this.isSet) {
-            		visItem.jumpToAnimationPercent(sunLocationAnim, ((currTime-sunrise)/(sunset-sunrise))*100);
-            		this.isSet=true;
-            	}
-                visItem.setObjectVisibility(sunObject, true);
-            } else {
-                visItem.setObjectVisibility(sunObject, false);
-            }
-        }/* *END* Sun position based on time */
+        //     var currDate = new Date();
+        //     var currTime = (currDate.getHours() + ":" + currDate.getMinutes()).split(/[.:]/);
+        //     hours = parseInt(currTime[0], 10);
+        //     minutes = currTime[1] ? parseInt(currTime[1], 10) : 0;
+        //     currTime = hours + minutes / 60;
+        //     if (currTime>=sunrise && currTime <=sunset) {
+        //     	if (!this.isSet) {
+        //     		visItem.jumpToAnimationPercent(sunLocationAnim, ((currTime-sunrise)/(sunset-sunrise))*100);
+        //     		this.isSet=true;
+        //     	}
+        //         visItem.setObjectVisibility(sunObject, true);
+        //     } else {
+        //         visItem.setObjectVisibility(sunObject, false);
+        //     }
+        // }/* *END* Sun position based on time */
 
 
         {/* *START* Battery drain based on data */
@@ -119,26 +119,26 @@ var house1 = {
     },
     init : function(visItem){
 
-        {/* *START* Set sun animation to represent day length and set to current time */
-            // average day length for november in london: 8:54
-            var day_length = "8:54".split(/[.:]/);
-            hours = parseInt(day_length[0], 10);
-            minutes = day_length[1] ? parseInt(day_length[1], 10) : 0;
-            day_length = hours + minutes / 60;
-            sunLocationName = ["SunAction"];
-            sunLocationAnim = visItem.getAnimationByName(sunLocationName);
-            visItem.updateTimeScale(sunLocationAnim, sunLocationAnim[0].duration/(day_length*3600));
+        // {/* *START* Set sun animation to represent day length and set to current time */
+        //     // average day length for november in london: 8:54
+        //     var day_length = "8:54".split(/[.:]/);
+        //     hours = parseInt(day_length[0], 10);
+        //     minutes = day_length[1] ? parseInt(day_length[1], 10) : 0;
+        //     day_length = hours + minutes / 60;
+        //     sunLocationName = ["SunAction"];
+        //     sunLocationAnim = visItem.getAnimationByName(sunLocationName);
+        //     visItem.updateTimeScale(sunLocationAnim, sunLocationAnim[0].duration/(day_length*3600));
 
-            var sunrise = "7:18".split(/[.:]/);
-            var hours = parseInt(sunrise[0], 10);
-            var minutes = sunrise[1] ? parseInt(sunrise[1], 10) : 0;
-            sunrise = hours + minutes / 60;
-            var currDate = new Date();
-            var currTime = (currDate.getHours() + ":" + currDate.getMinutes()).split(/[.:]/);
-            currTimePercent = ((currTime-sunrise)/(day_length))*100;
-            if (currTimePercent<=100 && currTimePercent>=0)
-            	visItem.jumpToAnimationPercent(sunLocationAnim, currTimePercent);
-        }/* *END* Set sun animation to represent day length */
+        //     var sunrise = "7:18".split(/[.:]/);
+        //     var hours = parseInt(sunrise[0], 10);
+        //     var minutes = sunrise[1] ? parseInt(sunrise[1], 10) : 0;
+        //     sunrise = hours + minutes / 60;
+        //     var currDate = new Date();
+        //     var currTime = (currDate.getHours() + ":" + currDate.getMinutes()).split(/[.:]/);
+        //     currTimePercent = ((currTime-sunrise)/(day_length))*100;
+        //     if (currTimePercent<=100 && currTimePercent>=0)
+        //     	visItem.jumpToAnimationPercent(sunLocationAnim, currTimePercent);
+        // }/* *END* Set sun animation to represent day length */
 
         {/* *START* Add battery to opacity items */
             opacityNames = ["Shape_IndexedFaceSet019","Battery001"];
