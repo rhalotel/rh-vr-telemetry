e3ce41fc0fd7128ca1af3d71d563b6d690f4f3e7
marcoBS335 <56341836+marcoBS335@users.noreply.github.com>
2020-01-20 11:30:31 +0100

    battery house back to the roots

--- examples/vis/energyHouse/houseSpecModel.js
+++ examples/vis/energyHouse/houseSpecModel.js
@@ -43,7 +43,7 @@ var house1 = {
             if (!isNaN(batteryState)) {
                 maxBatteryState = 2;
                 // animation goes from 100% to 0% so animation on 0% percent is 100% of fuel
-                percentAnim = batteryState/maxBatteryState;
+                percentAnim = 100-(100/maxBatteryState)*batteryState;
                 visItem.jumpToAnimationPercent(batteryAnims, 1-percentAnim);
             }
         }/* *END* Battery drain based on data */
